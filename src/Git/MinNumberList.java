4d6500de1ee0b02720143cbea315d104d0fc45c6
Andrei Vorontsov <andreivorontsov3@gmail.com>
2022-06-02 20:36:16 +0300

    создал метод который принимает список

---
+++ src/Git/MinNumberList.java
@@ -0,0 +1,20 @@
+package Git;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Создать новый проект.
+ * Написать метод, который принимает список чисел и возвращает минимальное число из списка чисел.
+ * В main продемонстрировать работу этого метода. Закоммитить, затем запушить эту з-чу в репозиторий.
+ * Скинуть ссылку на этот репозиторий в чат, либо в личку.
+ */
+public class MinNumberList {
+
+   public Integer minList(ArrayList<Integer> list){
+       return Collections.min(list);
+    }
+
+}
+
